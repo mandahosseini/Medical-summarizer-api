eafe3a1b5da28855de641744ae0fbad3992d04ec
mandahosseini <34593119+mandahosseini@users.noreply.github.com>
2025-03-07 13:34:57 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 from pydantic import BaseModel
 from transformers import pipeline
@@ -14,3 +15,7 @@ class ArticleRequest(BaseModel):
 def summarize(article: ArticleRequest):
     summary = summarizer(article.text, max_length=150, min_length=50, do_sample=False)
     return {"summary": summary[0]["summary_text"]}
+
+# اجرای اپلیکیشن
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
